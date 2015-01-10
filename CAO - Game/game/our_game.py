609f8700317ebbca2a42c9e20b2dcddb5919a9c1
Sjoerd <SJOERD@LAPTOP>
2015-01-10 16:52:48 +0100

    Map created

--- CAO - Game/game/our_game.py
+++ CAO - Game/game/our_game.py
@@ -9,39 +9,97 @@ import pygame
 import os
 import math
 import array
-from tkinter.tix import Balloon
 
 # Constants
 OBJECT_SIZE = 8
+PROGRAM_SPEED = 60
                 
 #Map objects
 walls = []
 finishes = []
 holes = []
 
+map = [
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"W         W                                                W",
+"W         W                                                W",
+"WWWWWWWW  WWWWW                                            W",
+"W         W                                                W",
+"W H       W                                                W",
+"W     S   W                                                W",
+"W         W                                                W",
+"W         W                                                W",
+"W    H    W                                                W",
+"W         W                                                W",
+"W         W                                                W",
+"W        HW                                                W",
+"W  WWWWWWWW                                                W",
+"W                            F                             W",
+"W                                                          W",
+"WWWWWWWWWWW                  F                             W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"W                                                          W",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+]
+
 # Sprites
 sprite_wall = pygame.image.load('wall.png') 
-sprite_empty = pygame.image.load('empty.png')
-sprite_hole = pygame.image.load('hole.png')
-sprite_ball = pygame.image.load('bal.png')
-sprite_finish = pygame.image.load('finish.png')
+#sprite_empty = pygame.image.load('empty.png')
+#sprite_hole = pygame.image.load('hole.png')
+#sprite_ball = pygame.image.load('bal.png')
+#sprite_finish = pygame.image.load('finish.png')
 
 # Initialise pygame
 os.environ["SDL_VIDEO_CENTERED"] = "1"
 pygame.init()
-
-#Set up display
-screen = pygame.display.set_mode((640, 480))
-
-#Create the ball
-ball = Ball()
-ball_position = array('d', [16.0, 16.0])
-
+clock = pygame.time.Clock()
 
 class Ball(object):
     
-    def __init__(selfs):
-        self.rect = pygame.Rect(START_POSITION[0], START_POSITION[1])
+    def __init__(self, position):
+        ball_position[0] = position[0]
+        ball_position[1] = position[1]
+        self.rect = pygame.Rect(position[0], position[1], OBJECT_SIZE, OBJECT_SIZE)
         
     def move(self, x, y):
         if (x != 0):
@@ -94,4 +152,56 @@ class Hole(object):
         holes.append(self)
         self.rect = pygame.Rect(position[0], position[1], OBJECT_SIZE, OBJECT_SIZE)
 
+
+#Set up display
+screen = pygame.display.set_mode((640, 480))
+
+#Create the ball
+ball_position = [16.0, 16.0]
+
+#Create map
+x = y = 0
+for row in map:
+    for coll in row:
+        if coll == "W" :
+            Wall((x,y))
+        if coll == "E" :
+            Finish((x,y))
+        if coll == "S" :
+            ball = Ball((x,y))
+        x+= OBJECT_SIZE
+    y+= OBJECT_SIZE
+    x=0
+
+running = True
+
+while running:
+    
+    clock.tick(PROGRAM_SPEED)
+    
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            running = False
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+            running = False
+    
+    #Fill map
+    screen.fill((0,0,0))
+    for wall in walls:
+        pygame.draw.rect(screen, (255, 255, 255), wall.rect)
+        screen.blit(sprite_wall, wall.rect)    
+    for finish in finishes:
+        pygame.draw.rect(screen, (255, 255, 255), finish.rect)           
+    pygame.draw.rect(screen, (255, 200, 0), ball.rect)
+    
+    
+    pygame.display.flip()
+
+
+
+
+
+
+         
+         
 
